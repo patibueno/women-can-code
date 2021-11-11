71d0fc1153d144b81e2fccc41816f19804f69e0a
Pati Bueno <patibuennodev@gmail.com>
2021-11-11 19:32:41 -0300

    corrigindo a frase do bloco if else

--- aula3/exercicio7.js
+++ aula3/exercicio7.js
@@ -7,11 +7,11 @@ hojeVaiChover = true;
 if (hojeVaiChover) {
   console.log("Leve seu guarda-chuva, hoje vai chover");
 } else {
-  console.log("Fique tranquilo, hoje será um dia se sol");
+  console.log("Fique tranquilo, hoje será um dia de sol");
 }
 
 //opção ternário
 
 hojeVaiChover
   ? console.log("Leve seu guarda-chuva, hoje vai chover")
-  : console.log("Fique tranquilo, hoje será um dia se sol");
+  : console.log("Fique tranquilo, hoje será um dia de sol");

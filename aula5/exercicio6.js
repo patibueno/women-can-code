768c2bd94b07490f227f4046208c3a2bb7d3f51e
Pati Bueno <patibuennodev@gmail.com>
2021-11-13 10:31:39 -0300

    atualizando exercicios com o filter

--- aula5/exercicio6.js
+++ aula5/exercicio6.js
@@ -1,19 +1,4 @@
-//opção for
 
-const criarPares = (validaPares) => {
-  for (let indice = 0; indice < validaPares.length; indice++) {
-    if (validaPares[indice] % 2 === 0) {
-      console.log(validaPares[indice]);
-    }
-  }
-};
-
-const valoresNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-criarPares(valoresNum);
-
-//
-
-//opção Filter
 const meusNumerosPares = valoresNum.filter((numero) => {
   return numero % 2 === 0;
 });
